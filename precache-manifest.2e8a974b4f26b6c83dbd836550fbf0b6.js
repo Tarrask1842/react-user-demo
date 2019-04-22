49e333452abb4261562eb2fa1ae4e2d2228defb7
BrianD <tarrasqueaohk@gmail.com>
2019-04-22 23:15:25 +0700

    Updates

---
+++ precache-manifest.2e8a974b4f26b6c83dbd836550fbf0b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c07ff4f6ae0cf6622b19",
+    "url": "/Tarrasque18/react-user-demo/static/js/runtime~main.cc74933c.js"
+  },
+  {
+    "revision": "7b459e31e9c4acd10a7b",
+    "url": "/Tarrasque18/react-user-demo/static/js/main.0a478b85.chunk.js"
+  },
+  {
+    "revision": "f2e5e4da586cacaf656f",
+    "url": "/Tarrasque18/react-user-demo/static/js/2.78d89337.chunk.js"
+  },
+  {
+    "revision": "7b459e31e9c4acd10a7b",
+    "url": "/Tarrasque18/react-user-demo/static/css/main.3bea6aaf.chunk.css"
+  },
+  {
+    "revision": "f1f9ab6cfea9279ede2a99b26f4c1310",
+    "url": "/Tarrasque18/react-user-demo/index.html"
+  }
+];
